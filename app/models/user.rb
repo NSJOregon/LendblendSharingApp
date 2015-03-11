e3c144dd8052a25660983602e751301674c8e13b
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-03-11 23:10:07 +0300

    Express relationship between users and items
    Two key distinctions between the types of users we have here, lenders and borrowers have been
    expressed in the user model
    It is now possible to do 'User.borrowed_items to see all the borrowed items.
    This code 'User.items' will list all the items that belong to a user since by default they
    belong to a user. But to be able to create a list of lent items a scope will be used.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,19 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 
-    # Owner
-    has_many :items, foreign_key: :owner_id
-    has_many :owners, through: :items
+    # By default anyone who logs in and has items is looking to lend. That's our design constraint 
+    # for now.
+
+    # Lender
+    has_many :items, foreign_key: :lender_id
+    has_many :lenders, through: :items
 
 
+    # We are reversing the previous lending relationship to create a situation where we can
+    # the other side of the coin, i.e. if a lender lends an item then who owns it?
+    # The corresponding code is a reversal of the previous. 
+    
     # Borrower
     has_many :borrowed_items, foreign_key: :borrower_id, class_name: 'Item'
-    has_many :borrowers, through: :items 
-
+    has_many :borrowers, through: :borrowed_items 
 end
