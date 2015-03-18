85b10f470379e66c752c2b59b3d7943cd458c454
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-03-18 17:43:25 +0300

    Add classname to items lenders and borrowers

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,7 @@
 class Item < ActiveRecord::Base
 
-
-belongs_to :lender
-belongs_to :borrower
+belongs_to :lender, class_name: 'User'
+belongs_to :borrower, class_name: 'User'
 
 mount_uploader :picture, PictureUploader
 end
