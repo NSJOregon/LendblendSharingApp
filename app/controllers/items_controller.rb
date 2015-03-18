29604a4140af747afb313a87bc7f0a1147b95595
Alvin Kato J.R <alvinkatojr@gmail.com>
2015-03-18 17:09:04 +0300

    Fix typo

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! only: [:create, :update, :destroy]
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
   
   def index
     @items = Item.all
