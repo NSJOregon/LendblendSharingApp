aa56020f3e3d4e049f18df05e4b5ec64f2426987
NSJOregon <nsjoregon@gmail.com>
2015-03-31 14:25:41 -0700

    Mod gemfile to help heroku

--- db/migrate/20150329014214_change_borrowed_period_date_format_in_item_table.rb
+++ db/migrate/20150329014214_change_borrowed_period_date_format_in_item_table.rb
@@ -1,6 +1,6 @@
 class ChangeBorrowedPeriodDateFormatInItemTable < ActiveRecord::Migration
     def up
-      change_column :items, :borrowed_period, :datetime
+      change_column :items, :borrowed_period, 'integer USING CAST(column_name AS integer)'
     end
 
     def down
